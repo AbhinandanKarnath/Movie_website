e8f95bdca5ead75595ca146fff34cbd7f24e8962
Abhinandan Karanth <abhinandankaranth52@gmail.com>
2024-09-21 23:59:44 +0530

    added ppt and updated MovieDetails files

--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import "./css/MovieDetails.css"
+import "./MovieDetails.css"
 
 
 const MovieDetails = () => {
